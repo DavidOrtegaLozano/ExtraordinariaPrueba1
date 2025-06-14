ec8af9cb7aed17114aadd2802df558ac1d4e205d
David Ortega Lozano <davidortegagithub@gmail.com>
2025-06-14 17:25:17 +0200

    packages creados

---
+++ DOL/src/main/java/es/ufv/dis/back/Examen2/DOL/DolApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.Examen2.DOL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolApplication.class, args);
+	}
+
+}
---
+++ DOL/src/test/java/es/ufv/dis/back/Examen2/DOL/DolApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.Examen2.DOL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
